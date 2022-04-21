cef5b345e8f30b202ca5d88d41579ecc151ec1d3
angjaay <angga.angzai@gmail.com>
2022-04-21 17:45:29 +0700

    add profile activity

---
+++ app/src/main/java/studio/angzai/tugas_1_akb_10119123/ProfileActivity.java
@@ -0,0 +1,21 @@
+package studio.angzai.tugas_1_akb_10119123;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/*
+    Tanggal pengerjaan : 21 april 2022
+    NIM : 10119123
+    Nama : Angga Cahya Abadi
+    Kelas : IF-3
+*/
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
